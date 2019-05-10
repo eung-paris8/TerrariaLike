a44625e196e5ae7c777218ac530ca0bd5493dfb0
yukitsuo <yukitsuo@yukitsuo>
2019-05-10 10:31:51 +0200

    test

--- controlleur/Controlleur.java
+++ controlleur/Controlleur.java
@@ -3,8 +3,7 @@ package controlleur;
 public class Controlleur {
 	public static void main(String[] args) {
 		System.out.println("Hello !");
-		System.out.println("Yolo");
-		System.out.println("POURQUOI JAVAFX FONCTIONNE PAS");
+	
 	}
 	
 }
