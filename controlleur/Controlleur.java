bcaa9d6d37f33a96b763bfca6250f6bc2fca3c0f
eung-paris8 <39116117+eung-paris8@users.noreply.github.com>
2019-05-10 15:38:47 +0200

    Update Controlleur.java
    effacement des betises

--- controlleur/Controlleur.java
+++ controlleur/Controlleur.java
@@ -2,17 +2,7 @@ package controlleur;
 
 public class Controlleur {
 	public static void main(String[] args) {
-		System.out.println("Hello !");
-
-		System.out.println("gqfgdq");
-		System.out.println("fdgq");
-	
-
-		System.out.println("Yolo");
-		System.out.println("POURQUOI JAVAFX FONCTIONNE PAS");
-		System.out.println("JE SAIS PAS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		System.out.println("FDQFDQFDQ");
-
+		
 	}
 	
 }
