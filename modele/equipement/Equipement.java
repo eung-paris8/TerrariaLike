ac89be205689b33fd8a8c354ccb5f182338a390e
yukitsuo <yukitsuo@yukitsuo>
2019-05-13 21:18:18 +0200

    Création des équipements

---
+++ modele/equipement/Equipement.java
@@ -0,0 +1,20 @@
+
+public class Equipement {
+	private String nomEquipement;
+	private int ptsDeVie;
+	
+	
+	public Equipement(String nomEquipement, int ptsDeVie) {
+		this.nomEquipement = nomEquipement;
+		this.ptsDeVie = ptsDeVie
+	}
+	
+	public String getNomEquipement() {
+		return this.nomEquipement;
+		
+	}
+	
+	public int getPtsDeVie() {
+		return this.ptsDeVie;
+	}
+}
---
+++ modele/equipement/Pelle.java
@@ -0,0 +1,9 @@
+
+public class Pelle extends Equipement {
+	
+	
+	public Pelle(String nom) {
+		super(nom, 10);
+	}
+
+}
