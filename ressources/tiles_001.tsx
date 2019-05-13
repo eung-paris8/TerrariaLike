a3dfa160d22a54bd1ea02fcb10c552630c51be76
Decnox <loick.ca@hotmail.fr>
2019-05-13 17:02:31 +0200

    ajout des resources donc fichiers csv pour la map et les tileset utilisés

---
+++ ressources/tiles_001.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.04.25" name="tiles_001" tilewidth="32" tileheight="32" tilecount="27" columns="9">
+ <image source="../PixelAntasy-FREE-Pixel-Art/PixelAntasy FREE Pixel Art/Tiles/001/tiles_001.png" width="288" height="96"/>
+</tileset>
