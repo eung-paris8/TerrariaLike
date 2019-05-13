e88c91edc4a2916d18856c15f1207d9b670f7386
Eric <Eric@DESKTOP-8O6TC9B>
2019-05-13 17:16:34 +0200

    ajout de constructeur

--- modele/personnages/Joueur.java
+++ modele/personnages/Joueur.java
@@ -2,4 +2,9 @@ package modele.personnages;
 
 public class Joueur extends Personnages{
 
+	public Joueur(String nom, int vitesse, int x, int y) {
+		super(nom, vitesse, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
 }
