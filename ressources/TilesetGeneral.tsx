87045eb1bf0eeed3fd0274fae8c70249bc43de10
eung-paris8 <39116117+eung-paris8@users.noreply.github.com>
2019-05-14 14:23:11 +0200

    Delete TilesetGeneral.tsx

--- ressources/TilesetGeneral.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2019.05.08" name="TilesetGeneral" tilewidth="32" tileheight="32" tilecount="200" columns="25">
- <image source="../Pictures/Sans titre 2.png" width="800" height="256"/>
-</tileset>
